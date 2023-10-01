3f379e95305479077f0840b58e66dc0917db9f44
youssef ben Amor <60358308+blodbling@users.noreply.github.com>
2023-10-01 23:53:51 +0100

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,27 +1,77 @@
-public class Animal {
+public class Zoo {
+    //PROSIT 3
+    Animal[] animals;
+    String name, city;
+    final int nbrCages = 25; 
 
-    String family, name;
-    int age;
-    boolean isMammal;
+    int nbrAnimals;
 
-    public Animal() {
-
+    public Zoo() {
     }
 
-    public Animal(String family, String name, int age, boolean isMammal) {
-        this.family = family;
+    public Zoo(String name, String city) { 
+        animals = new Animal[nbrCages];
         this.name = name;
-        this.age = age;
-        this.isMammal = isMammal;
+        this.city = city;
     }
+
+    void displayZoo() {
+        System.out.println("Name: " + name + ", City: " + city + ", N° Cages/Animals: " + nbrCages);
+    }
+
+    boolean addAnimal(Animal animal) {
+        if (searchAnimal(animal) != -1)
+            return false;
+        if (nbrAnimals == nbrCages)
+            return false;
+        animals[nbrAnimals] = animal;
+        nbrAnimals++;
+        return true;
+    }
+    boolean isZooFull() {
+        return nbrAnimals >= nbrCages;
+    }
+    public static Zoo comparerZoo(Zoo z1, Zoo z2) {
+        if (z1.nbrAnimals > z2.nbrAnimals) {
+            return z1;
+        } else if (z2.nbrAnimals > z1.nbrAnimals) {
+            return z2;
+        } else {
+
+            return z1;
+        }
+    }
+    boolean removeAnimal(Animal animal) {
+        int indexAnimal = searchAnimal(animal);
+        if (indexAnimal == -1)
+            return false;
+        for (int i = indexAnimal; i < nbrAnimals; i++) {
+            animals[i] = animals[i + 1];
+            animals[nbrAnimals] = null;
+            this.nbrAnimals--;
+        }
+        return true;
+    }
+
+    void displayAnimals() {
+        System.out.println("Liste des animaux de " + name + ":");
+        for (int i = 0; i < nbrAnimals; i++) {
+            System.out.println(animals[i]);
+        }
+    }
+
+    int searchAnimal(Animal animal) {
+        int index = -1;
+        for (int i = 0; i < nbrAnimals; i++) {
+            if (animal.name == animals[i].name)
+                return i;
+        }
+        return index;
+    }
+
 
     @Override
     public String toString() {
-        return "Animal{" +
-                "family='" + family + '\'' +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", isMammal=" + isMammal +
-                '}';
+        return "Name: " + name + ", City: " + city + ", N° Cages/Animals: " + nbrCages;
     }
 }
